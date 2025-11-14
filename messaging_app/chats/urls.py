84cd5fe3f957ad9b13c50f677afb8a2341457a63
samuelIkoli <ayibanimiikoli@gmail.com>
2025-11-14 20:09:03 +0100

    urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
-from django.contrib import admin
 
 router = routers.DefaultRouter()
 router.register("conversations", ConversationViewSet, basename="conversations")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # REQUIRED: must contain "api/"
-    path('api/', include('chats.urls')),
+    path("", include(router.urls)),
+    path(
+        "conversations/<uuid:conversation_id>/messages/",
+        MessageViewSet.as_view({"get": "list", "post": "create"}),
+        name="conversation-messages"
+    ),
 ]
