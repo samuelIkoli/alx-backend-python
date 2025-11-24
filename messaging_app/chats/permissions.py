45bbb8d7a1ea07e831461053ad99f2d753f73a14
samuelIkoli <ayibanimiikoli@gmail.com>
2025-11-24 23:35:47 +0100

    task 1

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -2,14 +2,31 @@ from rest_framework import permissions
 from .models import Conversation, Message
 
 
-class IsConversationParticipant(permissions.BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     """
-    Allows access only to participants of the conversation.
+    Custom permission:
+    - Only allows authenticated users
+    - Only participants of a conversation can access messages or the conversation
     """
 
+    def has_permission(self, request, view):
+        # User must be authenticated to access the API
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        # obj is a Conversation instance
-        return request.user in obj.participants.all()
+        """
+        Object-level permission checks:
+        obj can be Conversation or Message.
+        """
+        # If accessing a Conversation object
+        if isinstance(obj, Conversation):
+            return request.user in obj.participants.all()
+
+        # If accessing a Message object
+        if isinstance(obj, Message):
+            return request.user in obj.conversation.participants.all()
+
+        return False
 
 
 class IsMessageParticipant(permissions.BasePermission):
